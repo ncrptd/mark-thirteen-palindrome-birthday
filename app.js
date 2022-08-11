6e53dad2bdf43e9a6d3aa8321083d207e8d9f806
Rockey Biswas <85108386+ncrptd@users.noreply.github.com>
2022-08-11 19:15:05 +0530

    function to get the next date

--- app.js
+++ app.js
@@ -21,12 +21,77 @@ function convertDateToStr(date) {
   strDate.year = date.year;
   return strDate;
 }
+
+function getAllDateVariations(date) {
+  let dateStr = convertDateToStr(date);
+  let ddmmyyyy = dateStr.day + dateStr.month + dateStr.year;
+  let mmddyyyy = dateStr.month + dateStr.day + dateStr.year;
+  let yyyymmdd = dateStr.year + dateStr.month + dateStr.day;
+  let ddmmyy = dateStr.day | (dateStr.month + dateStr.year.split(-2));
+  let mmddyy = dateStr.month + dateStr.day + dateStr.year.split(-2);
+  let yymmdd = dateStr.year.split(-2) + dateStr.month + dateStr.day;
+  return [ddmmyyyy, mmddyyyy, yyyymmdd, ddmmyy, mmddyy, yymmdd];
+}
+function checkPalindromeForAllDateFormats(date) {
+  let allFormatDates = getAllDateVariations(date);
+  let flag = false;
+  for (let i = 0; i < allFormatDates.length; i++) {
+    if (checkPalindrome(allFormatDates[i])) {
+      flag = true;
+      break;
+    }
+  }
+  return flag;
+}
+
+function checkLeapYear(year) {
+  if (year % 400 === 0) {
+    return true;
+  }
+  if (year % 100) {
+    return false;
+  }
+  if (year % 4 === 0) {
+    return true;
+  }
+  return false;
+}
 let date = {
-  day: 1,
-  month: 1,
-  year: 2020,
+  day: 29,
+  month: 2,
+  year: 2021,
 };
-console.log(convertDateToStr(date));
+
+function getNextDate(date) {
+  let day = date.day + 1;
+  let month = date.month;
+  let year = date.year;
+  let daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  if (month === 2) {
+    if (checkLeapYear) {
+      if (day > 29) {
+        day = 1;
+        month++;
+      } else {
+        if (day > 28) {
+          day = 1;
+          month++;
+        }
+      }
+    }
+  } else {
+    if (day > daysInMonth[month - 1]) {
+      day = 1;
+      month++;
+    }
+    if (month > 12) {
+      month = 1;
+      year++;
+    }
+  }
+}
+
+// function getNextPalindromeDate(date) {}
 // let dateInput = document.querySelector("#birth-date");
 // let showButton = document.querySelector(".show-button");
 // let output = document.querySelector(".output");
@@ -36,9 +101,9 @@ console.log(convertDateToStr(date));
 //   if (birthdayStr !== "") {
 //     let dateArr = birthdayStr.split("-");
 //     let date = {
-//       day: dateArr[2],
-//       month: dateArr[1],
-//       year: dateArr[0],
+//       day: +dateArr[2],
+//       month: +dateArr[1],
+//       year: +dateArr[0],
 //     };
 //   }
 // }
