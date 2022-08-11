df091d380214b6a813f4af716a34e9ddea2b00f8
Rockey Biswas <85108386+ncrptd@users.noreply.github.com>
2022-08-11 16:58:29 +0530

    added function to reverse string

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
-let dateInput = document.querySelector("#birth-date");
-let showButton = document.querySelector(".show-button");
-let output = document.querySelector(".output");
-
-function clickHandler() {
-  let birthdayStr = dateInput.value;
-  if (birthdayStr !== "") {
-    let dateArr = birthdayStr.split("-");
-    let date = {
-      day: +dateArr[2],
-      month: +dateArr[1],
-      year: +dateArr[0],
-    };
-    console.log(date);
-  }
+//function to reverse the string
+function reverseStr(str) {
+  return str.split("").reverse().join("");
 }
+console.log(reverseStr("hello"));
+// let dateInput = document.querySelector("#birth-date");
+// let showButton = document.querySelector(".show-button");
+// let output = document.querySelector(".output");
 
-showButton.addEventListener("click", clickHandler);
+// function clickHandler() {
+//   let birthdayStr = dateInput.value;
+//   if (birthdayStr !== "") {
+//     let dateArr = birthdayStr.split("-");
+//     let date = {
+//       day: dateArr[2],
+//       month: dateArr[1],
+//       year: dateArr[0],
+//     };
+//   }
+// }
+
+// showButton.addEventListener("click", clickHandler);
